f164398be54902347b1a11199c942e569f82392d
ahargey <3650393@myuwc.ac.za>
2019-01-30 08:40:42 +0200

    #first commit proper

---
+++ Mass and Age.R
@@ -0,0 +1,14 @@
+Mass and Age 
+29 January 2019 
+Ayesha Hargey
+3650393
+
+mass <- 48              # mass? 48
+mass <- mass * 2.0      # mass? 96
+age  <- 126             # age? 126
+age  <- age - 17        # age? 109
+mass_index <- mass/age  # mass_index? 0.88
+x <- 40 
+y <- 23
+z <- x-y #17
+z
