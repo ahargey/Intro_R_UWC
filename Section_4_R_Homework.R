d2e00b880e3aa5e93c47027a5c4bfaba734df7f4
ahargey <3650393@myuwc.ac.za>
2019-02-05 12:56:21 +0200

    COMPLETE

--- Section_4_R_Homework.R
+++ Section_4_R_Homework.R
@@ -22,30 +22,30 @@ any(is.na(iris)) #is there any missing data?
 summary(iris) #summary of the data according to quartiles and min/max values
 
 iris_SD <- iris %>% 
-  group_by(Species) %>%
+  group_by(Species) %>% #grouping by species
   select (Sepal.Length, Sepal.Width) %>% 
   summarise(mean_length = mean(Sepal.Length),  
             mean_width = mean(Sepal.Width),
-            var_sepal_length = var(Sepal.Length),
+            var_sepal_length = var(Sepal.Length), #calculating the variance
             var_sepal_width = var(Sepal.Width),
             n = n()) %>% 
-  mutate(se_sepal_length = sqrt(var_sepal_length/n),
+  mutate(se_sepal_length = sqrt(var_sepal_length/n), #calculating standard error
          se_sepal_width = sqrt(var_sepal_width/n))
 
-#RIBBON
-iris_ribbon <- ggplot(iris, aes(x = Sepal.Length, y = Petal.Length)) +
-  geom_ribbon(aes(ymin = mean(Sepal.Length) - 1.5, ymax = mean(Sepal.Length) + 1.5), fill = "black", alpha = 0.4) +
-  geom_point(aes(colour = Species)) +
-  geom_line(aes(colour = Species, group = Species)) +
-  scale_colour_manual(values = c("lightpink1", "salmon1", "tomato4")) +
-  labs(x = "Sepal Length (mm)", y = "Petal Length (mm)") +
-  ggtitle("Sepal and Petal Length Across Three Species of Iris") + #title
+#VOLCANO PLOT
+iris_volcano <- ggplot(data=iris, aes(x = Petal.Length)) + 
+  stat_density(aes(ymax = ..density..,  ymin = -..density.., #volcano plot emphasizes the density
+                       fill = Species, color = Species), 
+                   geom = "ribbon", position = "identity") +
+  facet_wrap(~Species) + coord_flip() + #flipping the co-ordinates for a more viewable graph
+  labs(x = "Petal Length", y = "Density") + #axis labels
+  ggtitle("Volcano Plot of Petal Length in 3 Species of Iris") + #titles
   theme_bw () +
   theme(axis.text.x = element_text(angle = 40, hjust = 1, colour = "black", size=12),
         axis.text.y = element_text(hjust = 1, colour = "black", size=12),
         plot.background = element_rect(fill = "#f0eae8"),
         plot.title = element_text(size=16, face="bold", hjust=0.5))
-iris_ribbon
+iris_volcano
 
 #URINE DATASET
 library(boot) #for the urine dataset 
@@ -63,14 +63,14 @@ any(is.na(urine)) #is there any missing data?
 summary(urine) #summary of the data according to quartiles and min/max values
 
 urine_round <- urine %>%
-  group_by(round(ph)) %>% 
-  select (-ph) %>%
-  summarise(mean_osmo = mean(osmo, na.rm = TRUE),
-            mean_urea = mean(urea, na.rm = TRUE)) 
+  group_by(round(ph)) %>% #rounding off the pH to a whole number
+  select (-ph) %>% #removing the original column
+  summarise(mean_osmo = mean(osmo, na.rm = TRUE), #removing na data
+            mean_urea = mean(urea, na.rm = TRUE)) #removing na data
 
 #DENSITY PLOT:
 urine_densityplot <- ggplot(urine, aes(x = urea)) +
-  geom_density(aes(fill = "maroon")) +
+  geom_density(aes(fill = "maroon")) + #fill of the density plot 
   labs(x = "Urea", y = "Density") +
   ggtitle("Density plot of urea concentration in urine") + 
   theme_bw () +
@@ -78,5 +78,6 @@ urine_densityplot <- ggplot(urine, aes(x = urea)) +
         axis.text.y = element_text(hjust = 1, colour = "black", size=12),
         plot.background = element_rect(fill = "#f0eae8"),
         plot.title = element_text(size=16, face="bold", hjust=0.5))
-urine_densityplot
+urine_densityplot #shows the distribution of the variable urea
+
 
